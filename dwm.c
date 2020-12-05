af48abab77f5ecca37959a80d050fb3cfe3a0557
MGC <chagel@gmail.com>
2020-12-04 21:00:20 -0800

    patch fullscreen

--- dwm.c
+++ dwm.c
@@ -226,6 +226,7 @@ static void sendmon(Client *c, Monitor *m);
 static void setclientstate(Client *c, long state);
 static void setfocus(Client *c);
 static void setfullscreen(Client *c, int fullscreen);
+static void fullscreen(const Arg *arg);
 static void setgaps(const Arg *arg);
 static void setlayout(const Arg *arg);
 static void setcfact(const Arg *arg);
@@ -1630,6 +1631,19 @@ setgaps(const Arg *arg)
 	p->realgap = MAX(p->realgap, 0);
 	p->gappx = p->realgap * p->isgap;
 	arrange(selmon);
+}
+
+Layout *last_layout;
+void
+fullscreen(const Arg *arg)
+{
+	if (selmon->showbar) {
+		for(last_layout = (Layout *)layouts; last_layout != selmon->lt[selmon->sellt]; last_layout++);
+    setlayout(&((Arg) { .v = &layouts[2] }));
+  } else {
+    setlayout(&((Arg) { .v = last_layout }));
+  }
+	togglebar(arg);
 }
 
 void
